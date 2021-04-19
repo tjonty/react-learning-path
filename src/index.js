c007ce7dd920c8a74862e4217ae5afc60e5f34cf
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2021-04-19 20:30:39 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>This is paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
