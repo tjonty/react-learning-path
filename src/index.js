62bedccbd88af9b25037f2fc5f0f02b4cee25abf
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2021-04-20 10:54:26 +0530

    inline style

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ var ReactDOM = require("react-dom");
 
 const name = "jonty";
 const number = 23;
+const customStyle = {
+  color: "black",
+  backgroundColor: "lightgreen",
+  border: "1px solid black",
+  padding: "10px",
+  textAlign: "center"
+};
 
 ReactDOM.render(
-  <div>
+  <div style={customStyle}>
     <h1>hello {name}</h1>
     <p>Your lucky number is {number}</p>
     <p>Your favourite number is {4 + 4}</p>
